c7f9ace11062a3c317daf24bbf810bd3219a16dd
Theologos Mavropoulos <rusinner88@gmail.com>
2023-03-11 19:29:37 +0200

    setting up auth

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
 const app = express();
-const connect = require("./db/connect");
+const connectDB = require("./db/connect");
+
+//routers
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/jobs");
 
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
@@ -12,9 +16,8 @@ app.use(express.json());
 // extra packages
 
 // routes
-app.get("/", (req, res) => {
-  res.send("jobs api");
-});
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -23,7 +26,8 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    connect(process.env.MONGO_URL);
+    //connect to DB
+    await connectDB(process.env.MONGO_URL);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
