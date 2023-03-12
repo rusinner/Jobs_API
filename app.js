bf22d3dde28881033d1556ed38cab7c08f313bc4
Theologos Mavropoulos <rusinner88@gmail.com>
2023-03-12 19:33:19 +0200

    final ready to deploy

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const app = express();
 const connectDB = require("./db/connect");
 const authenticateUser = require("./middleware/authentication");
 
+// extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+// const swaggerDocument = YAML.load("./swagger.yaml");
+
 //routers
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
@@ -13,7 +24,21 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust-proxy", 1);
+app.use(
+  rateLimiter({
+    windows: 15 * 60 * 1000,
+    max: 100,
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+app.get("/", (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
 
 // routes
 app.use("/api/v1/auth", authRouter);
---
+++ models/Job.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const jobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, "Please provide company name"],
+      maxlength: 50,
+    },
+    position: {
+      type: String,
+      required: [true, "Please provide position"],
+      maxlength: 100,
+    },
+    status: {
+      type: String,
+      enum: ["interview", "declined", "pending"],
+      default: "pending",
+    },
+    //this property links the job with the user created it
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please provide a user"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Job", jobSchema);
