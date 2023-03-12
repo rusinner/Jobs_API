466dcb6820c5d67e3b0ccbced69f91faf4d9c0a3
Theologos Mavropoulos <rusinner88@gmail.com>
2023-03-12 13:17:07 +0200

    login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,10 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   //create user
   const user = await User.create({ ...req.body });
-  //create token
-  const token = jwt.sign({ userId: user._id, naem: user.name }, "jwtsecret", {
-    expiresIn: "30d",
-  });
+  const token = user.createJWT();
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
